9c7faf4d4b1bf102e0146eee03c55abc1b574675
Bárbara <barbaradd33@gmail.com>
2026-08-29 18:21:16 -0400

    pequeños ajustes

---
+++ ContAsistencias/Pages/Reportes/VistaReporte.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContAsistencias.Pages.Reportes
+{
+    public class VistaReporteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
